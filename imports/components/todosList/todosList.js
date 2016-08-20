058f572351d53dce372a3f260e4ceb5593a56aee
William Gregory <mail@wmgregory.co.uk>
2016-08-20 19:49:33 +0100

    Added form to add a todo.

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -10,9 +10,27 @@ class TodosListCtrl {
 
     this.helpers({
       tasks() {
-        return Tasks.find({});
+
+        // Show newest tasks at the top
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
       }
     })
+  }
+
+  addTask(newTask) {
+
+    // Insert a task into the collection
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+
+    // Clear form
+    this.newTask = '';
   }
 }
 
