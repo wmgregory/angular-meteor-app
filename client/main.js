35df56329bb61c2bc5c39651f651d2c8a5d60ed0
William Gregory <mail@wmgregory.co.uk>
2016-08-20 18:58:08 +0100

    Added angular-ui & bootstrap with sass.

--- client/main.js
+++ client/main.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 import todosList from '../imports/components/todosList/todosList';
 
 angular.module('simple-todos', [
   angularMeteor,
+  uiRouter,
   todosList.name
 ]);
