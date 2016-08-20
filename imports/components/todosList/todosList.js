06f73335e6c6f4d2334a4ea5e59c6ef6e3aa16ef
William Gregory <mail@wmgregory.co.uk>
2016-08-20 19:15:12 +0100

    Added mongo db support

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,16 +1,18 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Tasks } from '../../api/tasks.js';
+
 import template from './todosList.html';
 
 class TodosListCtrl {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
   }
 }
 
@@ -18,5 +20,5 @@ export default angular.module('todosList', [
   angularMeteor
 ]).component('todosList', {
   templateUrl: 'imports/components/todosList/todosList.html',
-  controller: TodosListCtrl
+  controller: ['$scope', TodosListCtrl]
 });
